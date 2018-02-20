30d50bf479d49394c039293c11a49365d13a77b9
DYLAN WEIJERMANS - STUDENT <G00356261@gmit.ie>
2018-02-20 12:39:26 +0000

    Week 20 Lab - Ex1,2 and 3 20/02/2018

---
+++ Break_Ex2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Break_Ex2{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int counter = 0;
+
+    System.out.println("See if you can guess the correct number");
+    System.out.println("Gueass a number from 1 to 10");
+    System.out.println("You have 10 attempts");
+
+    while(input.hasNext()){
+		if(input.nextInt() == 8){
+			System.out.println("Match found, it is 8");
+      		break;
+    }
+    counter++;
+    	if(counter == 10){
+			System.out.println("No Match found");
+			break;
+		  }
+		}
+	}
+}
---
+++ Break_Ex3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Break_Ex3{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int counter = 0;
+
+    System.out.println("See if you can guess the correct number");
+    System.out.println("Gueass a number from 1 to 10");
+    System.out.println("You have 10 attempts");
+
+    while(input.hasNext()){
+		if((input.nextInt() == 8) || (counter ==10)){
+			System.out.println("Game over");
+      		break;
+    }
+
+		}
+	}
+}
