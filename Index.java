a9ca39517b8e1217b512c5b484e8daada966560e
DYLAN WEIJERMANS - STUDENT <G00356261@gmit.ie>
2018-03-07 13:54:24 +0000

    Week 22

--- Index.java
+++ Index.java
@@ -5,11 +5,12 @@ public class Index{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Select an option (1-3):");
+    System.out.println("Select an option (1-4):");
     System.out.println();
     System.out.println("1. Break");
     System.out.println("2. Switch");
-    System.out.println("3. Exit");
+    System.out.println("3. Methods");
+    System.out.println("4. Exit");
 
     System.out.println();
 
@@ -28,6 +29,11 @@ public class Index{
         FirstSwitch.main(null);
         break;
       case 3:
+	    System.out.println("Methods lab");
+	    System.out.println();
+	    Methods.main(null);
+       break;
+      case 4:
         System.out.println("Exiting....");
         break;
       default:
